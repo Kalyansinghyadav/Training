44bea9b27df3a3099120bd0a279874a5352872f3
Kalyansinghyadav <Kalyan.singh@berylsystems.com>
2019-09-13 16:09:57 +0530

    create actions in employees_controller

--- rails/Company/app/controllers/employees_controller.rb
+++ rails/Company/app/controllers/employees_controller.rb
@@ -8,10 +8,31 @@ class EmployeesController < ApplicationController
 	end
 
 	def create
+		employee = Employee.new
+    employee.name = params[:employee][:name]
+    employee.address = params[:employee][:address]
+    employee.contact_no = params[:employee][:contact_no]
+    employee.department_id = params[:employee][:department_id]
+    employee.save
+    redirect_to employees_path
 		
 	end
 
-	def update
-		
+	def edit
+		@employee = Employee.find(params[:id])
 	end
+
+	def show
+		@employee = Employee.find(params[:id])
+	end
+  
+  def update
+  	@employee = Employee.find(params[:id])
+  	if @employee.update(name:params[:employee][:name],address: params[:employee][:address],
+  		 contact_no:params[:employee][:contact_no],department_id:params[:employee][:department_id])
+       redirect_to employees_path
+    else
+    	render "edit"
+    end
+  end
 end
